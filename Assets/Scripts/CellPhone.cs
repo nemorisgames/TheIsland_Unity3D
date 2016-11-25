8ba3143b0883e2911b79ebdabeb8e475c971c503
felipeclaude <felipeclaude@hotmail.com>
2016-11-25 18:08:47 -0300

    implemented notification in cellphone. Weather manager base structure

--- Assets/Scripts/CellPhone.cs
+++ Assets/Scripts/CellPhone.cs
@@ -1,188 +1,233 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CellPhone : MonoBehaviour {
-    public Transform target;
-    GameObject cellphoneBody;
-    public float speed = 1f;
-    public float speedRotation = 1f;
-    public Vector3 position;
-    public Vector3 positionSelected; 
-    bool selected = false;
-    public Light light;
-    public Light lightStandBy;
-    bool inTransition = false;
-    public bool active = false;
+public class CellPhone : MonoBehaviour
+{
+	public Transform target;
+	GameObject cellphoneBody;
+	public float speed = 1f;
+	public float speedRotation = 1f;
+	public Vector3 position;
+	public Vector3 positionSelected;
+	bool selected = false;
+	public Light light;
+	public Light lightStandBy;
+	bool inTransition = false;
+	public bool active = false;
 
 
-    public enum CellphoneFunctions {Light, CameraPhoto, CameraVideo, ReviewPhotos, Call};
-    public CellphoneFunctions currentFunction = CellphoneFunctions.Light;
+	public enum CellphoneFunctions { Light, CameraPhoto, CameraVideo, ReviewPhotos, Call };
+	public CellphoneFunctions currentFunction = CellphoneFunctions.Light;
 
-    public Material cellphoneMaterialFunctions;
-    int indiceActual = 0;
+	public Material cellphoneMaterialFunctions;
+	int indiceActual = 0;
 
-    [Header("ForScreenShots")]
-    public TakePhoto photoFunctionality;
-    private bool isSavingPhoto = false;
-    private bool canUseMouseScroll = true;
+	[Header("ForScreenShots")]
+	public TakePhoto photoFunctionality;
+	private bool isSavingPhoto = false;
+	private bool canUseMouseScroll = true;
+	[Header("ForNotifications")]
+	public GameObject notification;
+	public AudioClip notificationSound;
+	public AudioSource source;
+	private float timeOut = 0;
+	private bool showingNotification = false;
 
-    void Start()
-    {
-        cellphoneMaterialFunctions.mainTextureOffset = new Vector2(0f, 0.032f);
-        cellphoneBody = transform.FindChild("Cuerpo").gameObject;
-        cellphoneBody.SetActive(active);
-    }
+	void Start()
+	{
+		cellphoneMaterialFunctions.mainTextureOffset = new Vector2(0f, 0.032f);
+		notification.SetActive(false);
+		cellphoneBody = transform.FindChild("Cuerpo").gameObject;
+		cellphoneBody.SetActive(active);
+	}
 
-    public void activate()
-    {
-        activate(true);
-    }
+	public void activate()
+	{
+		activate(true);
+	}
 
-    public void deactivate()
-    {
-        activate(false);
-    }
+	public void deactivate()
+	{
+		activate(false);
+	}
 
-    public void activate(bool active)
-    {
-        this.active = active;
-        cellphoneBody.SetActive(active);
-    }
-    void LateUpdate()
-    {
-        
-        if (!selected)
-        {
-            Vector3 tVec = new Vector3(target.position.x, target.position.y, target.position.z) + target.right * position.x + target.forward * position.z + target.up * position.y;
-            transform.position = Vector3.Lerp(transform.position, tVec, Time.deltaTime * speed);
-            transform.forward = Vector3.Lerp(transform.forward, target.forward, Time.deltaTime * speedRotation);
-        }
-        else
-        {
-            if (transform.parent != target)
-            {
-                Vector3 tVec = new Vector3(target.position.x, target.position.y, target.position.z) + target.right * positionSelected.x + target.forward * positionSelected.z + target.up * positionSelected.y;
-                transform.position = Vector3.Lerp(transform.position, tVec, Time.deltaTime * speed * 5f);
-                transform.forward = Vector3.Lerp(transform.forward, target.forward, Time.deltaTime * speedRotation * 10f);
-            }
-        }
+	public void activate(bool active)
+	{
+		this.active = active;
+		cellphoneBody.SetActive(active);
+	}
+	void LateUpdate()
+	{
 
-        if (!active)
-            return;
+		if (!selected)
+		{
+			Vector3 tVec = new Vector3(target.position.x, target.position.y, target.position.z) + target.right * position.x + target.forward * position.z + target.up * position.y;
+			transform.position = Vector3.Lerp(transform.position, tVec, Time.deltaTime * speed);
+			transform.forward = Vector3.Lerp(transform.forward, target.forward, Time.deltaTime * speedRotation);
+		}
+		else
+		{
+			if (transform.parent != target)
+			{
+				Vector3 tVec = new Vector3(target.position.x, target.position.y, target.position.z) + target.right * positionSelected.x + target.forward * positionSelected.z + target.up * positionSelected.y;
+				transform.position = Vector3.Lerp(transform.position, tVec, Time.deltaTime * speed * 5f);
+				transform.forward = Vector3.Lerp(transform.forward, target.forward, Time.deltaTime * speedRotation * 10f);
+			}
+		}
 
-        //Checkout on cellphone
-        if (Input.GetMouseButtonDown(1))
-        {
-            if (!inTransition)
-            {
-                selected = !selected;
-                if (selected)
-                {
-                    StartCoroutine(waitPosition());
-                    CaraFunctions.Instance.leftHand.SendMessage("Hide");
-                    CaraFunctions.Instance.rightHand.SendMessage("Show");
-                }
-                else
-                {
-                    transform.parent = null;
-                    CaraFunctions.Instance.leftHand.SendMessage("Show");
-                    CaraFunctions.Instance.rightHand.SendMessage("Hide");
-                }                    
-            }
-            ScreenManager.Instance.CloseScreen();
-            canUseMouseScroll = true;
-        }
+		if (!active)
+			return;
 
-        if(Input.GetAxis("Mouse ScrollWheel") != 0f && canUseMouseScroll)
-        {
-            if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-            {
-                nextFunction(true);
-            }
-            if (Input.GetAxis("Mouse ScrollWheel") > 0f)
-            {
-                nextFunction(false);
-            }
-        }
+		//Checkout on cellphone
+		if (Input.GetMouseButtonDown(1))
+		{
+			if (!inTransition)
+			{
+				selected = !selected;
+				if (selected)
+				{
+					StartCoroutine(waitPosition());
+					CaraFunctions.Instance.leftHand.SendMessage("Hide");
+					CaraFunctions.Instance.rightHand.SendMessage("Show");
+					WhenUsingCellPhone();
+				}
+				else
+				{
+					transform.parent = null;
+					CaraFunctions.Instance.leftHand.SendMessage("Show");
+					CaraFunctions.Instance.rightHand.SendMessage("Hide");
+				}
+			}
+			ScreenManager.Instance.CloseScreen();
+			canUseMouseScroll = true;
+		}
 
-        //Cellphone function
-        if (Input.GetMouseButtonDown(0) && selected)
-        {
-            switch (currentFunction) {
-                case CellphoneFunctions.Light:
-                    light.enabled = !light.enabled;
-                    AC.GlobalVariables.SetBooleanValue(0, light.enabled);
-                    break;
-			    case CellphoneFunctions.CameraPhoto:
-					StartCoroutine (takePhoto ());					
-                    break;
-                case CellphoneFunctions.ReviewPhotos:
-                    ScreenManager.Instance.ShowScreen(ScreenType.PhotoView);
-                    canUseMouseScroll = false;
-                    break;
-            }
-        }
+		if (Input.GetAxis("Mouse ScrollWheel") != 0f && canUseMouseScroll)
+		{
+			if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+			{
+				nextFunction(true);
+			}
+			if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+			{
+				nextFunction(false);
+			}
+		}
 
-        cellphoneMaterialFunctions.mainTextureOffset = new Vector2(0f, Mathf.Lerp(cellphoneMaterialFunctions.mainTextureOffset.y, (indiceActual * 0.2f + 0.032f), Time.deltaTime * 3f));
-    }
-    IEnumerator takePhoto()
-    {
-        float intensityOriginal = light.intensity;
-        float angleOriginal = light.spotAngle;
-        bool lightEnabledOriginal = light.enabled;
-        light.enabled = true;
-        
-        light.spotAngle = 140f;
-        lightStandBy.enabled = true;
-        while (light.intensity > intensityOriginal * 0.1f)
-        {
-            light.intensity -= intensityOriginal / 20f;
-            yield return new WaitForSeconds(0.01f);
-        }
+		//Cellphone function
+		if (Input.GetMouseButtonDown(0) && selected)
+		{
+			switch (currentFunction)
+			{
+				case CellphoneFunctions.Light:
+					light.enabled = !light.enabled;
+					AC.GlobalVariables.SetBooleanValue(0, light.enabled);
+					break;
+				case CellphoneFunctions.CameraPhoto:
+					StartCoroutine(takePhoto());
+					break;
+				case CellphoneFunctions.ReviewPhotos:
+					ScreenManager.Instance.ShowScreen(ScreenType.PhotoView);
+					canUseMouseScroll = false;
+					break;
+			}
+		}
 
-        yield return new WaitForSeconds(0.2f);
-        lightStandBy.enabled = false;
-        lightStandBy.intensity = 2f;
+		cellphoneMaterialFunctions.mainTextureOffset = new Vector2(0f, Mathf.Lerp(cellphoneMaterialFunctions.mainTextureOffset.y, (indiceActual * 0.2f + 0.032f), Time.deltaTime * 3f));
+	}
+	IEnumerator takePhoto()
+	{
+		float intensityOriginal = light.intensity;
+		float angleOriginal = light.spotAngle;
+		bool lightEnabledOriginal = light.enabled;
+		light.enabled = true;
 
-        while (light.intensity < intensityOriginal * 4f)
-        {
-            light.intensity += intensityOriginal * 4f / 5f;
-            yield return new WaitForSeconds(0.01f);
-            if (!isSavingPhoto)
-            {
-                isSavingPhoto = true;
-                photoFunctionality.SaveCameraScreenShot();
-            }            
-        }
-        yield return new WaitForSeconds(0.2f);       
-        light.spotAngle = angleOriginal;
-        light.intensity = intensityOriginal;
-        light.enabled = lightEnabledOriginal;
-        isSavingPhoto = false;
-    }
+		light.spotAngle = 140f;
+		lightStandBy.enabled = true;
+		while (light.intensity > intensityOriginal * 0.1f)
+		{
+			light.intensity -= intensityOriginal / 20f;
+			yield return new WaitForSeconds(0.01f);
+		}
 
-    void nextFunction(bool next)
-    {
-        indiceActual = Mathf.Clamp(indiceActual + (next ? 1 : -1), 0, 4);
-        switch (indiceActual)
-        {
-            case 0: currentFunction = CellphoneFunctions.Light; break;
-            case 1: currentFunction = CellphoneFunctions.CameraPhoto; break;
-            case 2: currentFunction = CellphoneFunctions.CameraVideo; break;
-            case 3: currentFunction = CellphoneFunctions.ReviewPhotos; break;
-            case 4: currentFunction = CellphoneFunctions.Call; break;
-        }
-    }
+		yield return new WaitForSeconds(0.2f);
+		lightStandBy.enabled = false;
+		lightStandBy.intensity = 2f;
 
-    IEnumerator waitPosition()
-    {
-        inTransition = true;
-        yield return new WaitForSeconds(.3f);
-        transform.parent = target;
-        Vector3 tVec = new Vector3(target.position.x, target.position.y, target.position.z) + target.right * positionSelected.x + target.forward * positionSelected.z + target.up * positionSelected.y;
-        transform.position = tVec;
-        transform.forward = target.forward;
-        transform.rotation = target.rotation;
-        inTransition = false;
-    }
+		while (light.intensity < intensityOriginal * 4f)
+		{
+			light.intensity += intensityOriginal * 4f / 5f;
+			yield return new WaitForSeconds(0.01f);
+			if (!isSavingPhoto)
+			{
+				isSavingPhoto = true;
+				photoFunctionality.SaveCameraScreenShot();
+			}
+		}
+		yield return new WaitForSeconds(0.2f);
+		light.spotAngle = angleOriginal;
+		light.intensity = intensityOriginal;
+		light.enabled = lightEnabledOriginal;
+		isSavingPhoto = false;
+	}
+
+	void nextFunction(bool next)
+	{
+		indiceActual = Mathf.Clamp(indiceActual + (next ? 1 : -1), 0, 4);
+		switch (indiceActual)
+		{
+			case 0: currentFunction = CellphoneFunctions.Light; break;
+			case 1: currentFunction = CellphoneFunctions.CameraPhoto; break;
+			case 2: currentFunction = CellphoneFunctions.CameraVideo; break;
+			case 3: currentFunction = CellphoneFunctions.ReviewPhotos; break;
+			case 4: currentFunction = CellphoneFunctions.Call; break;
+		}
+	}
+
+	IEnumerator waitPosition()
+	{
+		inTransition = true;
+		yield return new WaitForSeconds(.3f);
+		transform.parent = target;
+		Vector3 tVec = new Vector3(target.position.x, target.position.y, target.position.z) + target.right * positionSelected.x + target.forward * positionSelected.z + target.up * positionSelected.y;
+		transform.position = tVec;
+		transform.forward = target.forward;
+		transform.rotation = target.rotation;
+		inTransition = false;
+	}
+	void WhenUsingCellPhone()
+	{
+		if (showingNotification)
+		{
+			timeOut = Time.time;
+			InvokeRepeating("NotificationDuration", 0f, 1f);
+			showingNotification = false;
+			CancelInvoke("NotificationSound");
+		}
+	}
+	void ShowNotification()
+	{
+		notification.SetActive(true);
+		if (!showingNotification)
+		{
+			showingNotification = true; //can be used to close notification when she shows the cell phone
+			InvokeRepeating("NotificationSound", 0f, 1.5f);
+		}		
+	}
+	void NotificationSound()
+	{
+		//make sound
+		source.clip = notificationSound;
+		source.Play();		
+	}
+	void NotificationDuration()
+	{
+		//define max time
+		if (Time.time - timeOut >= 5f)
+		{
+			timeOut = 0;
+			CancelInvoke("NotificaionDuration");
+			notification.SetActive(false);			
+		}
+	}
 }
---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum Weather
+{
+	rain,
+	heavyRain,
+	Fog
+}
+public class WeatherManager : MonoBehaviour
+{
+	public static WeatherManager Instance = null;
+	void Awake()
+	{
+		Instance = this;
+	}
+	void RemoveOldWeather()
+	{
+
+	}
+	void StartNewWeather(int newWeather)
+	{
+		RemoveOldWeather();
+		switch (newWeather)
+		{
+			case (int)Weather.rain:
+				SetRain();
+				break;
+			case (int)Weather.heavyRain:
+				SetHeavyRain();
+				break;
+			case (int)Weather.Fog:
+				SetFog();
+				break;
+			default:
+				Debug.LogWarning("NO WEATHER GIVEN");
+				break;
+		}
+	}
+	void SetFog()
+	{
+		Debug.Log("setting fog");
+	}
+	void SetRain()
+	{
+		Debug.Log("setting rain");
+	}
+	void SetHeavyRain()
+	{
+		Debug.Log("setting heavy rain");
+	}
+}
