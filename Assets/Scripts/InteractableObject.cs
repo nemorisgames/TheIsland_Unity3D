f5c50fc60a6a273ed2427ea69d041da2e8f10cca
felipeclaude <felipeclaude@hotmail.com>
2016-11-27 19:47:37 -0300

    added falling tree demo scene and code

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum ObjectType
+{
+	tree,	
+}
+public class InteractableObject : MonoBehaviour
+{
+	[SerializeField]
+	ObjectType type;
+	[SerializeField]
+	Animator anim;
+	void Update()
+	{
+		
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		switch (type)
+		{
+			case ObjectType.tree:
+				if (collision.collider.tag == "stick")
+				{
+					Destroy(collision.gameObject);
+					anim.SetBool("falling", true);			
+				}
+				break;			
+			default:
+				break;
+		}
+	}
+}
