75ca5ab42a95e9940aeb20c5f5d845c41bae0e39
felipeclaude <felipeclaude@hotmail.com>
2016-12-07 10:00:01 -0300

    Fixed meta files and serialization (forced text and visible meta files). Inventory system and items (stick, matches,etc) work in progress

---
+++ Assets/Scripts/Stick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stick : InventoryItem {
+
+	protected override void Awake()
+	{
+		type = ItemType.stick;
+		base.Awake();
+	}
+	protected override void UseItem()
+	{
+		Debug.Log("using stick");
+	} 
+	//public override void Update()
+	//{
+	//	base.Update();
+	//}
+}
