a09f515b4decbd7e67870c50fa598736d7b711fe
FelipeClaude-PC\FelipeClaude <felipeclaude@hotmail.com>
2017-04-08 13:49:59 -0300

    fixed scene transition

--- Assets/Scripts/UI/SceneTransitionManager.cs
+++ Assets/Scripts/UI/SceneTransitionManager.cs
@@ -38,20 +38,27 @@ public class SceneTransitionManager : MonoBehaviour
 	IEnumerator waitForLoadedScene(int s)
 	{
 		float r = 0;
+		//Aqui va la animacion"
+		while (bg.color != Color.red)
+		{
+			bg.color = Color.Lerp(bg.color, Color.red, Time.deltaTime);
+			//Debug.Log(bg.color);
+		}
+		Debug.Log("Changing SCENE!");
 		AsyncOperation op = SceneManager.LoadSceneAsync(s, LoadSceneMode.Single);
 		while (!op.isDone)
 		{
-			r += Time.deltaTime * 5;
-			if (r > 255)
-				r = 255;
-			bg.color = new Color(r, bg.color.g, bg.color.b);
-			yield return new WaitForSeconds(0.2f);
+			Debug.Log("SCENE NOT READY");
+			yield return null;
 		}
-		yield return new WaitForSeconds(0.5f);
-		while (bg.color.a > 0)
-		{
-			bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, bg.color.a - Time.deltaTime * 0.2f);
-		}
+		//animacion de salida
+
+		//yield return new WaitForSeconds(0.5f);
+		//while (bg.color.a > 0)
+		//{
+		//	bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, bg.color.a - Time.deltaTime * 0.2f);
+		//}
 		bg.gameObject.SetActive(false);
+		Debug.Log("SCENE READY!");
 	}
 }
